72040d3f6611c2597e9ad2b3a2337eb1be6eea9a
juankm08-a11y <juancarlospabon01@gmail.com>
2025-04-01 04:24:37 -0500

    seventh commit

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function page() {
+export default function Page() {
   return (
     <div className="bg-amber-200 ">
       <h1 className="text-xl">Build</h1>
