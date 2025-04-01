f536a4cefd1003928b15441990c2d0e27cdc0cd2
juankm08-a11y <juancarlospabon01@gmail.com>
2025-04-01 04:32:17 -0500

    ten  commit

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="bg-amber-200 justify-center items-center ">
+    <div className="flex bg-amber-200 justify-center items-center ">
       <h1 className="text-xl">Build</h1>
     </div>
   );
